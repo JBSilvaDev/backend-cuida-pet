c7e8493dd988b9a1cb7be6fef893b7e3fb5ae60d
JBSilvaDev <jbsilva.dev@outlook.com>
2023-08-03 09:53:03 -0300

    update refresh token

--- lib/app/helpers/jwt_helper.dart
+++ lib/app/helpers/jwt_helper.dart
@@ -10,7 +10,7 @@ class JwtHelper {
     final claimSet = JwtClaim(
       issuer: 'cuidapet',
       subject: userId.toString(),
-      expiry: DateTime.now().add(Duration(seconds: 30)),
+      expiry: DateTime.now().add(Duration(days: 1)),
       notBefore: DateTime.now(),
       issuedAt: DateTime.now(),
       otherClaims: <String, dynamic>{'supplier': supplierId},
@@ -29,8 +29,7 @@ class JwtHelper {
         issuer: accessToken,
         subject: 'RefreshToken',
         expiry: DateTime.now().add(Duration(days: 20)),
-        // notBefore: DateTime.now().add(Duration(hours: 12)),
-        notBefore: DateTime.now(),
+        notBefore: DateTime.now().add(Duration(hours: 12)),
         issuedAt: DateTime.now(),
         otherClaims: <String, dynamic>{});
     return 'Bearer ${issueJwtHS256(claimSet, _jwtSecret)}';
